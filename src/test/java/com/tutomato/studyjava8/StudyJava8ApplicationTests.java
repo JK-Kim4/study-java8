1b7a1f08e7f6b6a6184c30310ddfec1ea1f28803
jongwan <jw.kim@changbi.com>
2022-08-29 15:26:27 +0900

    first init

---
+++ src/test/java/com/tutomato/studyjava8/StudyJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutomato.studyjava8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJava8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
