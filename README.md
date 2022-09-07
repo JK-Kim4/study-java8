5bc3009a34e08d54a6b180973031d9e1ff5d2a7e
jongwan <jongbell4@gmail.com>
2022-09-07 14:33:54 +0900

    day2

--- README.md
+++ README.md
@@ -21,3 +21,29 @@
      - 동일한 입력값에 대해 동일한 반환값을 보장하지 못한다면 함수형 프로그래밍 X
    - 고차함수
      - 함수가 함수를 파라미터로 받아서 함수를 리턴할 수 있다.
+     
+4. java.lang.function 패키지
+   - java에서 제공하는 함수형 인터페이스
+   - 자바에서 미리 정의해둔 자주 사용할만한 함수 인터페이스
+   - Function<T, R>
+     - T 타입을 받아 R 타입을 리턴하는 함수 인터페이스
+     - 함수 조합용 메소드
+       - compose
+       - andThan
+   - BiFunction<T, U, R>
+     - 두 개의 값 (T, U)을 받아 R타입으로 리턴하는 함수 인터페이스
+     - R apply(T t, U, u)
+   - Consumer<T>
+     - T 타입을 받아 아무것도 리턴하지 않는 함수 인터페이스
+     - 함수 조합용 메소드
+       - andThan
+   - Supplier<T>
+     - T 타입의 값을 제공하는 함수 인터페이스
+   - Predicate<T>
+     - T 타입의 값을 받아 true & false를 리턴
+     - 함수 조합용 메소드
+       - and()
+       - not()
+       - or()
+   - UnaryOperator<T>
+   - BinaryOperator<T>
--- src/main/java/com/tutomato/studyjava8/Foo.java
+++ src/main/java/com/tutomato/studyjava8/Foo.java
@@ -1,10 +1,12 @@
 package com.tutomato.studyjava8;
 
+import java.util.function.Function;
+
 public class Foo {
 
     public static void main(String[] args) {
 
-        // 익명 내부 클래스 형태로 함수형 인터페이스를 구현하여 사용 (Java 8 이전 문법)
+        /*// 익명 내부 클래스 형태로 함수형 인터페이스를 구현하여 사용 (Java 8 이전 문법)
         RunSomething rm1 = new RunSomething() {
             @Override
             public void doIt() {
@@ -23,7 +25,23 @@ public class Foo {
             System.out.println("hello rm3-1");
             System.out.println("hello rm3-2");
         };
-        rm3.doIt();
+        rm3.doIt();*/
+
+        /*Plus10 p10 = new Plus10();
+        System.out.println(p10.apply(10));*/
+
+        Function<Integer, Integer> plus10 = (i) -> i + 10;
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+        //System.out.println(multiply2.apply(2));
+
+        //함수형 메소드 조합
+        Function<Integer, Integer> compose = plus10.compose(multiply2);
+
+        System.out.println(compose.apply(30)); //70
+
+        //Function<Integer, Integer> andThan = plus10.andThen(multiply2);
+
+        System.out.println(plus10.andThen(multiply2).apply(30)); // 80
 
     }
 }
